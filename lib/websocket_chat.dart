b160590fb5f74a923d45acc9aa507fefe53e045b
Lukas Buck <Lukas.buck@e-mail.de>
2024-02-03 11:36:09 +0100

    add dependencis

---
+++ lib/websocket_chat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WebSocketChat extends StatelessWidget {
+  const WebSocketChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
