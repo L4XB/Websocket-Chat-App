b3fbbe14cc6de49754314f567d73b8565f1c2e69
Lukas Buck <Lukas.buck@e-mail.de>
2024-02-03 16:10:54 +0100

    updatet user mdoel

--- lib/source/domain/entities/message_model.dart
+++ lib/source/domain/entities/message_model.dart
@@ -1,14 +1,17 @@
 class MessageModel {
+  String id;
   String userName;
   String message;
 
-  MessageModel({required this.userName, required this.message});
+  MessageModel(
+      {required this.id, required this.userName, required this.message});
 
   factory MessageModel.fromJSON(Map<String, dynamic> data) {
-    return MessageModel(userName: data["userName"], message: data["message"]);
+    return MessageModel(
+        id: data["id"], userName: data["userName"], message: data["message"]);
   }
 
   Map<String, dynamic> toJSON() {
-    return {"userName": userName, "message": message};
+    return {"id": id, "userName": userName, "message": message};
   }
 }
